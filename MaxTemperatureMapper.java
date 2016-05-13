ff5dab73e1f9bca74059dfc397379ace9240b8bd
zhongnaner <deng_yuqi@yahoo.com>
2016-05-13 16:58:01 +0800

    Update MaxTemperatureMapper.java

--- MaxTemperatureMapper.java
+++ MaxTemperatureMapper.java
@@ -10,16 +10,10 @@ public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWr
 		public void map(LongWritable key, Text value, Context context)
 		throws IOException, InterruptedException {
 		String line = value.toString();
-		String year = line.substring(15, 19);
+		String year = line.substring(13, 17);
 		int airTemperature;
-		if (line.charAt(87) == '+') { // parseInt doesn't like leading plus signs
 		airTemperature = Integer.parseInt(line.substring(88, 92));
-		} else {
-		airTemperature = Integer.parseInt(line.substring(87, 92));
-		}
-		String quality = line.substring(92, 93);
-		if (airTemperature != MISSING && quality.matches("[01459]")) {
+		String quality = line.substring(19, 23);
 		context.write(new Text(year), new IntWritable(airTemperature));
-		}
 	}
 }
